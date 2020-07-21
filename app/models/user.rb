82e9ddce515e5925c83367942694f89c9d36fe13
sonotaku <takumasonoda0223@icloud.com>
2020-07-21 18:18:05 +0900

    messagesテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :name, presence: true
 
 # アソシエーション
-  # has_many :messages
   has_many :room_users
   has_many :rooms, through: :room_users
+  has_many :messages
 end
